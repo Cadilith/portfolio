215825747075d2a3cacbdda371b4963e6f6eba15
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-07-20 15:42:56 +0200

    project page base layout

--- src/app/projects/[projectId]/page.js
+++ src/app/projects/[projectId]/page.js
@@ -7,46 +7,42 @@ export default function Projects({ params }) {
   const data = datas.filter((d) => d.id === params.projectId);
   return (
     <main className={styles.main}>
-      <h1>{data[0].title}</h1>
+      <div className={styles.main__title__wrapper}>
+        <h1>{data[0].title}</h1>
+      </div>
       <article className={styles.project}>
         <h2 className={styles.project__title}>{data[0].title}</h2>
         <h3 className={styles.project__subtitle}>{data[0].subtitle}</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td>{data[0].description1}</td>
-              <td>
-                <Image
-                  src={data[0].pictures[0]}
-                  height={400}
-                  width={500}
-                  alt="project preview"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Image
-                  src={data[0].pictures[1]}
-                  height={200}
-                  width={500}
-                  alt="project preview"
-                />
-              </td>
-              <td rowSpan="2">{data[0].description2}</td>
-            </tr>
-            <tr>
-              <td>
-                <Image
-                  src={data[0].pictures[2]}
-                  height={200}
-                  width={500}
-                  alt="project preview"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className={styles.project__wrapper}>
+          <section className={styles.project__text}>
+            <p>{data[0].description1}</p>
+          </section>
+          <section className={styles.project__img}>
+            <Image
+              src={data[0].pictures[0]}
+              height={350}
+              width={800}
+              alt="project preview"
+            />
+          </section>
+          <section className={styles.project__img}>
+            <Image
+              src={data[0].pictures[1]}
+              height={175}
+              width={800}
+              alt="project preview"
+            />
+            <Image
+              src={data[0].pictures[2]}
+              height={175}
+              width={800}
+              alt="project preview"
+            />
+          </section>
+          <section className={styles.project__text}>
+            <p>{data[0].description2}</p>
+          </section>
+        </div>
         <a href={data[0].repository}>Link to repository</a>
       </article>
     </main>
