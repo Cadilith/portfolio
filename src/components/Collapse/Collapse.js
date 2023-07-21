63dffaa2d6f5145696d92ed6971645076eb1d65b
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-07-21 10:46:14 +0200

    delete useRef on collapse

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -1,14 +1,12 @@
 'use client';
 import styles from './Collapse.module.css';
 import { useState } from 'react';
-import { useRef } from 'react';
 
 function Collapse(props) {
   const [open, setOpen] = useState(true);
   const toggle = () => {
     setOpen(!open);
   };
-  const contentRef = useRef();
 
   return (
     <div className={styles.collapse__container}>
@@ -32,7 +30,6 @@ function Collapse(props) {
         className={`${styles.collapse__parent} ${
           open ? styles.show : styles.hide
         }`}
-        ref={contentRef}
         style={open ? { height: '70px' } : { height: '0px' }}
       >
         <div className={styles.collapse__content}>{props.children}</div>
