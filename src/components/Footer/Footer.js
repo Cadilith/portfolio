d8c891fb5c57a7e6410b5449699c2071aa3c0436
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-07-24 10:44:24 +0200

    footer mention edit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,8 @@
 import styles from './Footer.module.css';
 export default function Footer() {
-  return <footer className={styles.footer}>© Tiffanie Orsoni - 2023</footer>;
+  return (
+    <footer className={styles.footer}>
+      © Tiffanie Orsoni - 2023 All rights reserved
+    </footer>
+  );
 }
