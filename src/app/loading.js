71ae4d3fb667736012ce57a3bd21202ba1af3329
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-08-11 14:32:52 +0200

    loading in main tag

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,57 +3,59 @@ export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <>
-      <div class="hexagon" aria-label="Animated hexagonal ripples">
-        <div class="hexagon__group">
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
+      <main>
+        <div class="hexagon" aria-label="Animated hexagonal ripples">
+          <div class="hexagon__group">
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+          </div>
+          <div class="hexagon__group">
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+          </div>
+          <div class="hexagon__group">
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+          </div>
+          <div class="hexagon__group">
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+          </div>
+          <div class="hexagon__group">
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+          </div>
+          <div class="hexagon__group">
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+            <div class="hexagon__sector"></div>
+          </div>
         </div>
-        <div class="hexagon__group">
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-        </div>
-        <div class="hexagon__group">
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-        </div>
-        <div class="hexagon__group">
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-        </div>
-        <div class="hexagon__group">
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-        </div>
-        <div class="hexagon__group">
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-          <div class="hexagon__sector"></div>
-        </div>
-      </div>
-      <p aria-label="Loading">Loading</p>
+        <p aria-label="Loading">Loading</p>
+      </main>
     </>
   );
 }
